aee8d5c2eb5e6cc3113fd0ec43c011c4fe1f8a6b
Sam Berning <shberning@gmail.com>
2019-03-30 17:18:19 -0400

    Added server support and endpoints

--- api/fever.py
+++ api/fever.py
@@ -5,23 +5,52 @@ from handlers import *
 app = Flask(__name__)
 
 @app.route("/user/<string:username>", methods=["GET", "POST"])
-def users(username):
+def handle_users(username):
     if request.method == "GET":
         return get_user_data(username)
     if request.method == "POST":
-        return update_user_data(request.form)
+        return create_user(username)
+
+@app.route("/user/<string:username>/achievements", methods=["GET", "POST"])
+def handle_user_achievements(username):
+    if request.method == "GET":
+        return get_user_achievements(username)
+    if request.method == "POST":
+        return add_user_achievements(username, request.get_json())
+
+#@app.route("/user/<string:username>/achievements/progress", methods=["GET"])
+#def check_achievement_progress(username):
+    #    
+
+@app.route("/user/<string:username>/tags", methods=["GET", "POST"])
+def handle_user_tags(username):
+    if request.method == "GET":
+        return get_user_tags(username)
+    if request.method == "POST":
+        return add_user_tags(username, request.get_json())
 
 @app.route("/events", methods=["GET", "POST"])
-def events():
+def handle_events():
     if request.method == "GET":
         date = request.args.get('date', '')
         return get_events(date)
     if request.method == "POST":
-        return add_event(request.form)
+        return add_event(request.get_json())
 
-@app.route("/suggestions/<string:username>", methods=["GET"])
-def suggestions(username):
+@app.route("/achievements", methods=["GET", "POST"])
+def handle_achievements():
     if request.method == "GET":
-        return get_suggestions(username)
+        #return get_achievements()
+        return json.dumps(
+                {"result": "success",
+                "achievements": [
+                    {"name": "Jack of All Trades",
+                     "description": "Attended events in 5 different categories"},
+                    {"name": "Superfan", "description":
+                     "Attended 20 games or events for one sports team"}
+                    ]
+                })
+    if request.method == "POST":
+        return add_achievement(request.get_json())
 
 app.run("0.0.0.0", 3737)
--- api/handlers.py
+++ api/handlers.py
@@ -4,42 +4,39 @@
 import json 
 import psycopg2
 
-conn = psycopg2.connect("dbname=club-fever user=postgres password=rathin")
-conn_user = psycopg2.connect("dbname=club-fever user=postgres password=rathin")
-
+conn = psycopg2.connect("dbname=club_fever user=postgres password=postgres")
 cur = conn.cursor()
 
 
-
 def get_user_data(username):
-    #cur.execute("SELECT * FROM users WHERE username = " + "'username'")
-    
-    cur.execute("SELECT * from users WHERE username = '" + username + "'")
+    cur.execute("SELECT * from users WHERE username = '{}'".format(username))
     rows = cur.fetchall()
 
     print(rows[0])
 
+# test
+#get_user_data("rathination")
 
-get_user_data("rathination")
+# creates user with given username
+def create_user(username):
+    cur.execute("SELECT username from users WHERE username = '{}'".format(username))
+    rows = cur.fetchall()
+    if len(rows) != 0:
+        return {"result": "failure",
+                "error": "username '{}' already taken".format(username)}
 
-def update_user_data(form):
-        query = "UPDATE users SET username = %s, tags = %s, points = %i, badges_unlocked = %s WHERE username = '" + form["username"] + "'"
-        cur.execute(query, 
-        (
-        form["tags"],
-        form["points"],
-        form["badges_unlocked"]
-        ))
+    query = "INSERT INTO users (username, points) VALUES ('{}', 0)".format(username)
+    cur.execute(query)
+    return {"result": "success", "username": username}
 
-    return "success"
-
-def get_events():
+def get_events(date):
     event_template = {
     "id": 0,
     "event_name": "default", 
     "description": "default", 
-    "start_time" : "default",
-    "end_time" : "default",
+    "date": "1970-01-01",
+    "start_time": "default",
+    "end_time": "default",
     "tags": [], 
     "point_value": 0,
     "completed": False
@@ -47,23 +44,27 @@ def get_events():
 
     events_holder = []
 
-    cur.execute("SELECT * from events")
+    cur.execute("SELECT * from events WHERE date = '{}'".format(date))
     rows = cur.fetchall()
 
     for row in rows:
         event_template["id"] = row[0]
         event_template["event_name"] = row[1]
         event_template["description"] = row[2]
-        event_template["start_time"] = row[3]
-        event_template["end_time"] = row[4]
-        event_template["tags"] = row[5].split(",")
-        event_template["point_value"] = row[6]
-        event_template["completed"] = 0
-        events_holder.append(events_holder)
+        event_template["date"] = row[3]
+        event_template["start_time"] = row[4]
+        event_template["end_time"] = row[5]
+        event_template["tags"] = row[6]
+        event_template["point_value"] = row[7]
+        event_template["completed"] = row[8]
 
-    return events_holder
+        events_holder.append(event_template)
+
+    return {"result": "success", "events": events_holder}
     
-test = get_events()
+# test
+#test = get_events('2019-03-31')
+#print(test)
 
 def add_event(form):
     insert_query = "INSERT INTO events(event_name, description, start_time, end_time, tags, point_value, completed) VALUES (%s,%s,%s,%s,%s,%i,%i)"
